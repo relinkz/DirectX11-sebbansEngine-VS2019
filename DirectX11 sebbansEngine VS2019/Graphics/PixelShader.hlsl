0702d4d84bd818e6d835f8f1d01b0d48cd69d019
Sebastian Lundgren <relinkz@users.noreply.github.com>
2023-12-06 14:58:45 +0100

    Minor cleanup in pixelshader

--- DirectX11 sebbansEngine VS2019/Graphics/PixelShader.hlsl
+++ DirectX11 sebbansEngine VS2019/Graphics/PixelShader.hlsl
@@ -16,12 +16,6 @@ SamplerState objSamplerState : SAMPLER : register(s0);
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	// use texture coordinates
-  float3 pixelColor = objTexture.Sample(objSamplerState, input.inTexCoord);
-	return float4(pixelColor, alpha);
-	//return float4(input.inColor, alpha);
-	
-	//float3 pixelColor = float3(input.inTexCoord, 0.0f);
-	return float4(input.inTexCoord, 0.0f, 1.0f);
-	// use colors
-  return float4(input.inColor, 1.0f);
+  float3 diffuseColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+	return float4(diffuseColor, alpha);
 }
